059f9ef37eb3ea1858ac42382de09f7e2bf18833
DullSmile <973915422@qq.com>
2016-05-12 20:34:38 -0700

    chapter 2.6 Flask-Script扩展示例代码

---
+++ examples/hello2.6.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+from flask import Flask
+# Flask扩展，使用Flask-Script支持命令行选项
+from flask_script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    return '<h1>Hello workd!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return "<h1>hello, %s</h1>" %name
+
+if __name__ == ('__main__'):
+    manager.run()
