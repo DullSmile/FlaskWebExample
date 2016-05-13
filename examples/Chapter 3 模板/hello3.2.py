32dfc508f2edb0de408aab9ebaa1eded481910fc
DullSmile <973915422@qq.com>
2016-05-12 21:24:56 -0700

    chapter3.2 加入Flask-Bootstrap模板

---
+++ examples/Chapter 3 模板/hello3.2.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+#引入Flask  、模板、Bootstrap
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
