705234ad3d1eabe094a600a09eaa53bd1e7ff271
DullSmile <973915422@qq.com>
2016-05-14 06:05:20 -0700

    chapter6 略

---
+++ examples/Chapter6/hello.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from flask_mail import Mail
+
+app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+mail = Mail(app)
+
